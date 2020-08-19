0bcc0ea674f457da58fdfdf96773c9823a1185ab
CQBB <yangfangboster@gmail.com>
2020-08-19 00:12:53 -0400

    init repo

---
+++ src/main/java/com/weather/ApiGatewayRequest.java
@@ -0,0 +1,9 @@
+package com.weather;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApiGatewayRequest {
+    public String body;
+    public Map<String, String> queryStringParameters = new HashMap<>();
+}
---
+++ src/main/java/com/weather/WeatherEvent.java
@@ -0,0 +1,20 @@
+package com.weather;
+
+public class WeatherEvent {
+    public String locationName;
+    public Double temperature;
+    public Long timestamp;
+    public Double longitude;
+    public Double latitude;
+
+    public WeatherEvent() {
+    }
+
+    public WeatherEvent(String locationName, Double temperature, Long timestamp, Double longitude, Double latitude) {
+        this.locationName = locationName + "new change";
+        this.temperature = temperature;
+        this.timestamp = timestamp;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+}
