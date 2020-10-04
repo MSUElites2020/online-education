34b2bb3de4fc21113a94d6672e79d6ac591b7aa1
Alpha-Chuan <alphajinchuan@gmail.com>
2020-10-04 11:54:57 -0700

    Jinchuan User register service

--- src/main/java/com/online_education/user/server/UserComponent.java
+++ src/main/java/com/online_education/user/server/UserComponent.java
@@ -1,10 +1,12 @@
 package com.online_education.user.server;
 
+import com.online_education.dagger.AWSResourceModule.DynamoDBModule;
+import com.online_education.dagger.ObjectMapperModule;
 import dagger.Component;
 import javax.inject.Singleton;
 
 @Singleton
-@Component
+@Component(modules = {DynamoDBModule.class, ObjectMapperModule.class})
 public interface UserComponent {
   void inject(UserServiceHandler userServiceHandler);
 }
