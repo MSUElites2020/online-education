4b473d449aa70449dd4faaa2c533f76c6cb033bf
CQBB <yangfangboster@gmail.com>
2021-01-30 21:41:45 -0500

    用signed http请求

--- src/main/java/com/online_education/dagger/AWSResourceModule/ElasticSearchModule.java
+++ src/main/java/com/online_education/dagger/AWSResourceModule/ElasticSearchModule.java
@@ -1,20 +1,43 @@
 package com.online_education.dagger.AWSResourceModule;
 
+import com.amazonaws.auth.AWS4Signer;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.http.AWSRequestSigningApacheInterceptor;
 import dagger.Module;
 import dagger.Provides;
 import javax.inject.Singleton;
 import org.apache.http.HttpHost;
+import org.apache.http.HttpRequestInterceptor;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 
 @Module
 public class ElasticSearchModule {
+  private static final String HOST_NAME =
+      "search-msu-online-education-2-tnjwcrvx7dbwbmolrw6za44fru.us-east-1.es.amazonaws.com";
+  private static final int HOST_PORT = 443;
+  private static final String SCHMEME = "https";
+  private static final String SERVICE_NAME = "es";
+  // TODO to provide region constant via dagger
+  private static final String REGION = "us-east-1";
+  private static final AWSCredentialsProvider AWS_CREDENTIALS_PROVIDER =
+      new DefaultAWSCredentialsProviderChain();
+
   @Provides
   @Singleton
   RestHighLevelClient restHighLevelClient() {
+    AWS4Signer signer = new AWS4Signer();
+    signer.setServiceName(SERVICE_NAME);
+    signer.setRegionName(REGION);
+
+    RestClientBuilder restClientBuilder = RestClient.builder(new HttpHost(HOST_NAME, HOST_PORT, SCHMEME));
+
+    HttpRequestInterceptor interceptor =
+        new AWSRequestSigningApacheInterceptor(SERVICE_NAME, signer, AWS_CREDENTIALS_PROVIDER);
     return new RestHighLevelClient(
-        RestClient.builder(
-            new HttpHost(
-                "search-msu-online-education-2-tnjwcrvx7dbwbmolrw6za44fru.us-east-1.es.amazonaws.com", 443, "https")));
+        restClientBuilder.setHttpClientConfigCallback(
+            httpAsyncClientBuilder -> httpAsyncClientBuilder.addInterceptorLast(interceptor)));
   }
 }
